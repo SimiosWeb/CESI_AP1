877b4c7223bbc6ad14905f2114e6790622759e09
Arthur Doreau <arthurdoreau76@gmail.com>
2018-03-12 15:00:17 +0100

    nav

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<html>
+<head><link rel="stylesheet" href="assets/css/main.css"></head>
+</html>
 <?php
 include './include/header.php';
+
+$page = $_GET['page'] ?? "";
+$files = glob("./include/*.inc.php");
+var_dump($files);
+
 include './include/footer.php';
